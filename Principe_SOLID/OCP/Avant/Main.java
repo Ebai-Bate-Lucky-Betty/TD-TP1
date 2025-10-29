2a611867786f251aa6397c8626a0b83bfd9c2589
Ebai-Bate-Lucky-Betty <ebaibateluckybetty@gmail.com>
2025-10-29 18:33:15 +0100

    Version ne respectant le principe OCP

---
+++ Principe_SOLID/OCP/Avant/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        AreaCalculator calc = new AreaCalculator();
+        Rectangle r = new Rectangle(4, 3);
+        Circle c = new Circle(2);
+        System.out.println("Rectangle area = " + calc.calculateArea(r));
+        System.out.println("Circle area = " + calc.calculateArea(c));
+    }
+}
